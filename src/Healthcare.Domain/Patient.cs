76d2553995477f3795642c4b1e6962d98c223023
jose_almir <almir2560@gmail.com>
2023-09-12 21:01:09 -0300

    Property abstract

--- src/Healthcare.Domain/Patient.cs
+++ src/Healthcare.Domain/Patient.cs
@@ -4,7 +4,10 @@ namespace Healthcare.Domain
     public sealed class Patient : Audit
     {
         public string Name { get; set; }
-        
+
+        public override bool IsActive 
+            => !DeletedIn.HasValue;
+
         public Patient(string name)
         {
             Name = name;
